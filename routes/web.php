c0913073fca113ef364720f572bcc510caf4a10b
nia febriani <nia.it389@gmail.com>
2022-04-27 11:09:59 +0700

    penambahan controller web nia

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ Route::prefix('admin')->group(function () {
 
     Route::get('/jsIntroduction', 'JsintroController@index')->name('jsintro');
     Route::get('/jsIntroduction_next', 'JsintroController@index_next')->name('jsintro_next');
-  
+    
+    Route::get('/jsNia', 'NiaController@nia')->name('jsnia');
 
+    
+    
 });
